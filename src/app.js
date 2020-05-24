e6eb941b149a52736d5da8b267ed02b5fbe1ba15
Laercio Akamine Toma <laercioat@gmail.com>
2020-05-23 23:14:11 -0300

    desafio 02

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const {uuid, isUuid} = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  if (!isUuid(id)){
+    return response.status(400).json({error: "ID invalid."});
+  }
+
+  const repIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repIndex < 0){
+    return response.status(400).json({error: "Repository does not exist."});
+  }
+
+  repositories[repIndex].title = title;
+  repositories[repIndex].url = url;
+  repositories[repIndex].techs = techs;
+
+  return response.json(repositories[repIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  if (!isUuid(id)){
+    return response.status(400).json({error: "ID invalid."});
+  }
+  const repIndex = repositories.findIndex(repository => repository.id === id)
+  if (repIndex < 0){
+    return response.status(400).json({error: "Repository does not exist."});
+  }
+  repositories.splice(repIndex);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+//  const {likes} = request.body;
+
+  if (!isUuid(id)){
+    return response.status(400).json({error: "ID invalid."});
+  }
+  const repIndex = repositories.findIndex(repository => repository.id === id)
+  if (repIndex < 0){
+    return response.status(400).json({error: "Repository does not exist."});
+  }
+  repositories[repIndex].likes += 1;
+  return response.json(repositories[repIndex]);
 });
 
 module.exports = app;
